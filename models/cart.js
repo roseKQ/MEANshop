ffa75d5c4d4e7e1baeec3dc4f47c74738001f245
Rose <rose.kane@neueda.com>
2017-04-10 16:06:28 +0100

    changes to cart.js

--- models/cart.js
+++ models/cart.js
@@ -4,6 +4,12 @@ this.totalQty = oldCart.totalQty || 0;
 this.totalPrice = oldCart.totalPrice || 0;
 //this.discount = oldCart.discount || "";
 
+/* need to add registered helper function to use variable from html in handlebars*/
+
+
+this.discountCode = document.getElementById('discountCode');
+ 
+
 this.add = function (item, id) {
 
 var storedItem = this.items[id]
@@ -11,8 +17,28 @@ if(!storedItem){
     storedItem = this.items[id] = {item: item, qty: 0, price: 0};
 }
 storedItem.qty++;
-storedItem.price = storedItem.item.price* storedItem.qty;
+storedItem.price = storedItem.item.price * storedItem.qty;
 this.totalQty++;
+//add the totalprice function here 
+this.totalPrice = function (discountCode){
+
+//var totalPrice = this.totalPrice;
+
+if(discount!=null){
+    if(discount==="FIVE"){
+     totalPrice == this.totalPrice-5;
+     
+    }else if (discount==="TEN" && this.totalPrice>50){
+ totalPrice == this.totalPrice - 10;
+    }else if (discount==="FIFTEEN" && this.totalPrice>75){
+       totalPrice == this.totalPrice - 15;
+    }else {
+        totalPrice == this.totalPrice;
+    }
+    return totalPrice;
+}
+};
+
 this.totalPrice +=storedItem.item.price;
 
 };
@@ -20,14 +46,21 @@ this.totalPrice +=storedItem.item.price;
 //the apply discount is proving tricky in handlebars - will follow up but for now it is not functional
 
 /*
+this.totalPrice = function (discountCode){
+
+//var totalPrice = this.totalPrice;
+
 if(discount!=null){
     if(discount==="FIVE"){
-        this.totalPrice-5;
+     totalPrice =  this.totalPrice-5;
+     return totalPrice;
     }else if (discount==="TEN" && this.totalPrice>50){
-this.totalPrice - 10;
+ totalPrice = this.totalPrice - 10;
+ return totalPrice
     }else if (discount==="FIFTEEN" && this.totalPrice>75){
-        this.totalPrice - 15;
+       return this.totalPrice - 15;
     }
+}
 };*/
 
 this.reduceByOne = function(id) {
